2dc5a47a9621d09639d5e6d3f1f85c7de1fdd0cd
jds7232 <jds7232@tamu.edu>
2022-04-26 03:28:10 -0500

    stinky

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
 require("./app/routes/event.routes")(app);
 require("./app/routes/organization.routes")(app);
 
-const PORT = process.env.PORT || 8082;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
